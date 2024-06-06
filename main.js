9d5eb52902542bf2325b571f42dda796f0af0365
SculptTechProject <sculpttechproject@gmail.com>
2024-06-06 22:13:28 +0200

    #

--- main.js
+++ main.js
@@ -1,19 +1,32 @@
-if (window.history.replaceState) {
-  window.history.replaceState(null, null, window.location.href);
-}
-
-function Book(id, title, author, pages, read){
-      this.id = id;
-      this.title = title;
-      this.author = author;
-      this.pages = pages;
-      this.toogleRead = function () {
-            this.read = !this.read;
-      };
+function book(id, title, author, pages, read) {
+  this.id = id;
+  this.author = author;
+  this.title = title;
+  this.pages = pages;
+  this.toggleRead = function () {
+    this.read = read;
+  };
 }
 
 const library = {
-      "books": [],
-      "table": document.querySelector("book-table"),
-      "add book": function () {  }
-}
+  books: [],
+  table: document.querySelector("#book-table tbody"),
+  addBook: function (title, author, pages, read, id) {
+    const book = new Book(
+      (title = title),
+      (author = author),
+      (pages = pages),
+      (read = read),
+      (id = id)
+    );
+    this.books.push(book);
+  },
+  deleteBook: function (id) {
+    // get index of book to delete based on id
+    const index = this.books.findIndex((book) => {
+      return book.id === id;
+    });
+    this.books.splice(index, 1);
+  },
+};
+
